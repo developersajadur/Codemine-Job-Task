5d860c4cfc70903c1b320f62e8a643970675b482
developersajadur <itzmesojib@gmail.com>
2025-04-14 01:39:14 +0600

    try to fix error

--- src/components/Loaders/Loader.tsx
+++ src/components/Loaders/Loader.tsx
@@ -1,21 +1,17 @@
-"use client"
+'use client';
 
-import Lottie from 'react-lottie';
+import { Player } from '@lottiefiles/react-lottie-player';
 import animationData from '@/assets/Jsons/loader.json';
 
 const Loader = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true, 
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    };
-
     return (
-        <div className='h-screen w-full flex items-center justify-center bg-transparent'>
-            <Lottie options={defaultOptions} height={400} width={400} />
+        <div className="h-screen w-full flex items-center justify-center bg-transparent">
+            <Player
+                autoplay
+                loop
+                src={animationData}
+                style={{ height: '400px', width: '400px' }}
+            />
         </div>
     );
 };
